828cc77ee35508916ece7fed0620aaee71868f5d
Manna-na <gksskrn0158@gmail.com>
2023-07-30 18:53:52 +0900

    [4] feat: LoginForm 컴포넌트 분리

--- Hanna Kim/react-toypj/src/components/login/loginForm.js
+++ Hanna Kim/react-toypj/src/components/login/loginForm.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import LoginFormInput from "./LoginFormInput";
+import LoginFormButton from "./LoginFormButton";
 
 const LoginForm = () => {
   const [id, setId] = useState("");
@@ -8,12 +10,12 @@ const LoginForm = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    async function logJSONData() {
+    async function fetchUserData() {
       const response = await fetch("/data/user.json");
       const jsonData = await response.json();
       setUser(jsonData);
     }
-    logJSONData();
+    fetchUserData();
   }, []);
 
   const onChangeId = (e) => {
@@ -45,57 +47,27 @@ const LoginForm = () => {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
-            <div>
-              <label
-                htmlFor="email"
-                className="flex text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  onChange={onChangeId}
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            <LoginFormInput
+              label="Email address"
+              type="email"
+              id="email"
+              name="email"
+              autoComplete="email"
+              value={id}
+              onChange={onChangeId}
+            />
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  onChange={onChangePwd}
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            <LoginFormInput
+              label="Password"
+              type="password"
+              id="password"
+              name="password"
+              autoComplete="current-password"
+              value={pwd}
+              onChange={onChangePwd}
+            />
 
-            <div>
-              <button
-                onClick={() => clickLogin()}
-                type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Sign in
-              </button>
-            </div>
+            <LoginFormButton onClick={clickLogin} label="Sign in" />
           </form>
         </div>
       </div>
