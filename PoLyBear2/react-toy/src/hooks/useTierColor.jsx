24087e751db151e43610993115eb4175468c7012
임대영 <ldy092000@gmail.com>
2023-07-31 12:17:13 +0900

    [#11] feat 미사용 함수 제거

--- PoLyBear2/react-toy/src/hooks/useTierColor.jsx
+++ PoLyBear2/react-toy/src/hooks/useTierColor.jsx
@@ -1,22 +1,18 @@
 const useTierColor = (tier) => {
-  const getTierColor = (tier) => {
-    switch (tier) {
-      case 'queen':
-        return 'bg-pink-500';
-      case 'king':
-        return 'bg-purple-500';
-      case 'jack':
-        return 'bg-blue-500';
-      case 'joker':
-        return 'bg-red-500';
-      case 'ace':
-        return 'bg-green-500';
-      default:
-        return 'bg-gray-500';
-    }
-  };
-
-  return getTierColor(tier);
+  switch (tier) {
+    case 'queen':
+      return 'bg-pink-500';
+    case 'king':
+      return 'bg-purple-500';
+    case 'jack':
+      return 'bg-blue-500';
+    case 'joker':
+      return 'bg-red-500';
+    case 'ace':
+      return 'bg-green-500';
+    default:
+      return 'bg-gray-500';
+  }
 };
 
 export default useTierColor;
