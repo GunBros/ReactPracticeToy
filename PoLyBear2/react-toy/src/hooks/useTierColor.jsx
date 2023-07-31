2b2eef772a1521a3d6163ee46380478112d08ff9
임대영 <ldy092000@gmail.com>
2023-07-31 10:23:26 +0900

    #11 로비 페이지 구현-리뷰 반영

--- PoLyBear2/react-toy/src/hooks/useTierColor.jsx
+++ PoLyBear2/react-toy/src/hooks/useTierColor.jsx
@@ -1,35 +1,22 @@
-import {useEffect, useState} from "react";
-
 const useTierColor = (tier) => {
+  const getTierColor = (tier) => {
+    switch (tier) {
+      case 'queen':
+        return 'bg-pink-500';
+      case 'king':
+        return 'bg-purple-500';
+      case 'jack':
+        return 'bg-blue-500';
+      case 'joker':
+        return 'bg-red-500';
+      case 'ace':
+        return 'bg-green-500';
+      default:
+        return 'bg-gray-500';
+    }
+  };
 
-
-    const [tierColorClass, setTierColorClass] = useState('');
-
-    useEffect(() => {
-        let colorClass;
-        switch (tier) {
-            case 'queen':
-                colorClass = 'bg-pink-500';
-                break;
-            case 'king':
-                colorClass = 'bg-purple-500';
-                break;
-            case 'jack':
-                colorClass = 'bg-blue-500';
-                break;
-            case 'joker':
-                colorClass = 'bg-red-500';
-                break;
-            case 'ace':
-                colorClass = 'bg-green-500';
-                break;
-            default:
-                colorClass = 'bg-gray-500';
-        }
-        setTierColorClass(colorClass);
-    }, [tier]);
-
-    return tierColorClass;
+  return getTierColor(tier);
 };
 
 export default useTierColor;
