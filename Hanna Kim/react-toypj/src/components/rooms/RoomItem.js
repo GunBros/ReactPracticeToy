0b9d6cff85dab448399cb963d4217388af791410
Hanna Kim <gksskrn0158@gmail.com>
2023-07-31 14:24:44 +0900

    [#13] feat: 티어별 방 색상 변화 추가

--- Hanna Kim/react-toypj/src/components/rooms/RoomItem.js
+++ Hanna Kim/react-toypj/src/components/rooms/RoomItem.js
@@ -1,14 +1,25 @@
 import React from "react";
 import HostTier from "./HostTier";
 
+const HostTierColor = {
+  jack: "#C4A164",
+  queen: "#FFD689",
+  king: "#FFD700",
+  ace: "#E58632",
+  joker: "#D8D8D8",
+};
+
 const RoomItem = ({ item, navigate }) => {
   return (
     <div key={item.roomNumber} className="w-1280 p-3">
-      <div className="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 lg:w-200 border shadow shadow-lg">
+      <div
+        className="flex flex-col lg:flex-row rounded overflow-hidden h-auto lg:h-32 lg:w-200 shadow shadow-lg"
+        style={{backgroundColor: HostTierColor[item.hostTier]}}
+      >
         <HostTier hostTier={item.hostTier} />
         <div
-          className="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
-          style={{ width: "400px" }}
+          className="rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal"
+          style={{ width: "400px",  backgroundColor: HostTierColor[item.hostTier]  }}
         >
           <div className="text-black font-bold text-xl mb-2 leading-tight text-left">
             {item.roomTitle}
